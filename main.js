4bb8e3f1eb40907fc3b3cd5622f410c8289d6856
Karingomgroblox14 <74888422+karinaomgroblox14@users.noreply.github.com>
2021-12-05 20:42:08 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -22,7 +22,7 @@ console.error(error);
 else{
 console.log(results);
 document.getElementById("result_object_name").innerHTML = results[0].label;
-document.getElementsById("result_object_accuracy").innerHTML = results[0].confidence.toFixed(3);
+document.getElementById("result_object_accuracy").innerHTML = results[0].confidence.toFixed(3);
 }
 }
 
